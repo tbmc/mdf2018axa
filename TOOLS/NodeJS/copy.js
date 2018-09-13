57d74d91b3e0883cd929ef339b4e375340161715
Rémi FRUTEAU DE LACLOS <remi.fruteaudelaclos@axa.fr>
2018-09-13 18:00:02 +0200

    fix missing bundle file

--- TOOLS/NodeJS/copy.js
+++ TOOLS/NodeJS/copy.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 const {copy} = require("copy-paste");
-
-const file = fs.readFileSync(path.resolve("./dist/bundle.js"), {encoding: "utf8"});
-copy(file, () => console.log("COPY OK!"));
+const filePath = path.resolve("./dist/bundle.js");
+fs.lstat(filePath, (err) => {
+    if (!err) {
+        fs.readFile(filePath, {encoding: "utf8"}, (err, file) => {
+            copy(file, () => console.log("COPY OK!"));
+        });
+    }
+});
