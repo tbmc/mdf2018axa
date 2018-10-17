831516e1297fc592215bcf74a46b7600451f9639
Johnathan MEUNIER <johnathan2693@gmail.com>
2018-10-17 16:24:49 +0200

    solution fresque John + gridStart

--- TOOLS/NodeJS/src/tools.js
+++ TOOLS/NodeJS/src/tools.js
@@ -10,7 +10,20 @@ export const rotateMatrixCounterClockwise = matrix => flipMatrix(matrix).reverse
 
 export const flipMatrixCounterClockwise = matrix => rotateMatrix(matrix).reverse();
 
-export const toGrid = (input, separator="") => input.map((line) => line.split(separator).map((col) => col));
-export const toGridNumber = (input, separator="") => input.map((line) => line.split(separator).map((col) => col/1));
+export const toGrid = (input, separator = "") => input.map((line) => line.split(separator).map((col) => col));
+export const toGridNumber = (input, separator = "") => input.map((line) => line.split(separator).map((col) => col / 1));
 
 export const fillGrid = (size, val) => Array(size).fill().map(() => Array(size).fill(val));
+
+export const findStart = (grid, val) => {
+    let pos = [];
+    grid.some((line, lineIndex) => {
+        line.some((cell, colIndex) => {
+            if (cell == val) {
+                pos = [lineIndex, colIndex];
+                return true;
+            }
+        })
+    });
+    return pos;
+}
