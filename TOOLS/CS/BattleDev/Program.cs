2f4cdd9a604c2a8c4e1e1120a579f229fd0e8bc7
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2019-07-04 10:07:46 +0200

    updated cs tool to netcore

--- TOOLS/CS/BattleDev/Program.cs
+++ TOOLS/CS/BattleDev/Program.cs
@@ -1,7 +1,32 @@
-﻿// //  -----------------------------------------------------------------------// //  <copyright file="Program.cs" company="AXA France Service">// //      Copyright (c) AXA France Service. All rights reserved.// //  </copyright>// //  <actor>S821036 (VIGNERON Jean-baptiste)</actor>// //  <created>08/11/2016 18:20</created>// //  <modified>28/03/2017 14:26</modified>// //  -----------------------------------------------------------------------namespace BattleDev{    using System;    using System.Collections.Generic;    using System.Linq;    public class Program    {        public static void Main(string[] args)        {
+﻿// //  -----------------------------------------------------------------------
+// //  <copyright file="Program.cs" company="AXA France Service">
+// //      Copyright (c) AXA France Service. All rights reserved.
+// //  </copyright>
+// //  <actor>S821036 (VIGNERON Jean-baptiste)</actor>
+// //  <created>08/11/2016 18:20</created>
+// //  <modified>28/03/2017 14:26</modified>
+// //  -----------------------------------------------------------------------
+
+namespace BattleDev
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             // Placez votre code ici
 
             // ************************
-
+            Console.ReadLine();
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+            }
             //résultat
-            Console.WriteLine();        }    }}
+            Console.WriteLine();
+        }
+    }
+}
