8a34a916f9d380b63cbeacccc1d1d8ea1cdb659c
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2019-08-26 11:43:32 +0200

    update cs tool for dotnet core

--- TOOLS/CS/BattleDev/Program.cs
+++ TOOLS/CS/BattleDev/Program.cs
@@ -1,29 +1,15 @@
-﻿// //  -----------------------------------------------------------------------
-// //  <copyright file="Program.cs" company="AXA France Service">
-// //      Copyright (c) AXA France Service. All rights reserved.
-// //  </copyright>
-// //  <actor>S821036 (VIGNERON Jean-baptiste)</actor>
-// //  <created>08/11/2016 18:20</created>
-// //  <modified>28/03/2017 14:26</modified>
-// //  -----------------------------------------------------------------------
+﻿using System;
 
 namespace BattleDev
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            // Placez votre code ici
-
-            // ************************
-            Console.ReadLine();
             string line;
             while ((line = Console.ReadLine()) != null)
             {
+                Console.WriteLine(Math.Pow(int.Parse(line), 2));
             }
             //résultat
             Console.WriteLine();
